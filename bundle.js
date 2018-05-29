1bbe532e0f122bda4c8c897956df5d4ab8b4609a
John Dillon Easter <johndilloneaster@gmail.com>
2018-05-28 18:58:08 -0700

    Expanded to 10x10 grid with more colors and a color indicator

--- bundle.js
+++ bundle.js
@@ -2,13 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
   const grid = Array.from(document.querySelectorAll('div .border'))
   const colors = Array.from(document.querySelectorAll('div.select'))
-  const indicator = Array.from(document.querySelectorAll('div.indicator'))
+  const indicator = Array.from(document.querySelectorAll('div .indicator'))
   let selected = ''
 
   colors.forEach((color) => {
     color.addEventListener('click', function (event) {
       selected = event.target.classList[1]
-      indicator.forEach(square => square.classList.remove())
+      indicator.forEach(square => square.classList.remove(Array.from(square.classList)[1]))
       indicator.forEach(square => square.classList.add(`${selected}`))
     })
   })
