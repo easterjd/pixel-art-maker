785e493a41b740e2a6bbef43e68c80bf7b282266
John Dillon Easter <johndilloneaster@gmail.com>
2018-05-28 18:28:11 -0700

    Created 5x5 pixel drawer with color selector

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const grid = Array.from(document.querySelectorAll('div .border'))
+  const colors = Array.from(document.querySelectorAll('div.select'))
+  const indicator = Array.from(document.querySelectorAll('div.indicator'))
+  let selected = ''
+
+  colors.forEach((color) => {
+    color.addEventListener('click', function (event) {
+      selected = event.target.classList[1]
+      indicator.forEach(square => square.classList.remove())
+      indicator.forEach(square => square.classList.add(`${selected}`))
+    })
+  })
+
+
+  grid.forEach((square) => {
+    square.addEventListener('click', function (event) {
+      if (event.target.classList.contains(`${selected}`) === true) {
+        event.target.classList.remove(`${selected}`)
+      } else {
+        event.target.classList.add(`${selected}`)
+      }
+    })
+  })
+
+
+
+})
