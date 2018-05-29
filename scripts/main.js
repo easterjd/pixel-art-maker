c60470f5fcd1ba83fdfa62129c4f632cb66f6411
John Dillon Easter <johndilloneaster@gmail.com>
2018-05-28 19:54:36 -0700

    Seperated redering, color data, and interface scripts. Currently unworking

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,33 @@
+const gridColors = require('/grid-colors')
+const colorGrid = gridColors.colorGrid
+const selected = gridColors.selected
+const render = require('/render')
+
 document.addEventListener('DOMContentLoaded', function () {
   const grid = Array.from(document.querySelectorAll('div .border'))
   const colors = Array.from(document.querySelectorAll('div.select'))
   const indicator = Array.from(document.querySelectorAll('div .indicator'))
-  let selected = ''
+  let selected = 'WHITE'
 
   colors.forEach((color) => {
     color.addEventListener('click', function (event) {
       selected = event.target.classList[1]
-      indicator.forEach(square => square.classList.remove(Array.from(square.classList)[1]))
-      indicator.forEach(square => square.classList.add(`${selected}`))
+      render()
+      // indicator.forEach(square => square.classList.remove(Array.from(square.classList)[1]))
+      // indicator.forEach(square => square.classList.add(`${selected}`))
     })
   })
 
 
   grid.forEach((square) => {
     square.addEventListener('click', function (event) {
-      if (event.target.classList.contains(`${selected}`) === true) {
-        event.target.classList.remove(`${selected}`)
-      } else {
-        event.target.classList.add(`${selected}`)
-      }
+      colorGrid[grid.indexOf(event.target)] = selected
+      render()
+      // if (event.target.classList.contains(`${selected}`) === true) {
+      //   event.target.classList.remove(`${selected}`)
+      // } else {
+      //   event.target.classList.add(`${selected}`)
+      // }
     })
   })
 
---
+++ scripts/render.js
@@ -0,0 +1,27 @@
+const gridColors = require('/grid-colors')
+const colorGrid = gridColors.colorGrid
+const selected = gridColors.selected
+
+function render () {
+  const grid = Array.from(document.querySelectorAll('div .border'))
+  const indicator = Array.from(document.querySelectorAll('div .indicator'))
+
+  for (let i = 0; i < colorGrid.length; i++) {
+    let square = colorGrid[i]
+    if (Array.from(grid[i].classList).length === 3) {
+      grid[i].classList.remove(Array.from(grid[i].classList)[3])
+    } else {
+      grid[i].classList.add(colorGrid[i])
+    }
+  }
+  indicator.forEach( square => {
+    if (Array.from(square.classList).length === 2) {
+      square.classList.remove(Array.from(square.classList)[2])
+    } else {
+      square.classList.add(selected)
+    }
+  })
+}
+
+
+module.exports = render
